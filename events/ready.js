0a628d68e10bd12edaf515753f9232ac46773326
dennis0324 <dennis0324@naver.com>
2020-01-13 02:22:30 +0900

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -4,14 +4,14 @@ module.exports = bot =>{
     console.log(`${bot.user.username} is online`);
     // bot.user.setActivity("명령어에 귀 기울이는 중",{type: "STREAMING"});
     //
-    const allMembers = bot.users
-    console.log(`${allMembers.size}`)
-    allMembers.forEach(element => {
-        if(element.roles.find(r => r.name === "Bot")){
-            console.log("found bot!")
-        }
-    });
-    console.log(`${bot_count}`);
+    // const allMembers = bot.users
+    // console.log(`${allMembers.size}`)
+    // allMembers.forEach(element => {
+    //     if(element.roles.some(r => r.name === "Bot")){
+    //         console.log("found bot!")
+    //     }
+    // });
+    // console.log(`${bot_count}`);
 
     let statuses = [
         `${bot.guilds.size}!`,
