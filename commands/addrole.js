29e2f966ca728d5e2c39481619de74aaaca42cca
dennis0324 <dennis0324@naver.com>
2020-01-13 09:18:35 +0900

    Update addrole.js

--- commands/addrole.js
+++ commands/addrole.js
@@ -36,7 +36,7 @@ module.exports.run = async(bot, message, args) =>{
             .setAuthor(`알림`,message.guild.iconURL)
             .setDescription(`${rolename}역할이 성공적으로 추가되었습니다.`)
             .setFooter("도우미",null)
-            message.channel.send({embed: addRoleEmbed})
+            message.channel.send(addRoleEmbed)
         }
         catch(e){
             console.log(e.stack);
