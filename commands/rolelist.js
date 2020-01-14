ee5820c2ac726b2927861356bd5c1b336f660150
dennis0324 <dennis0324@naver.com>
2020-01-15 03:34:54 +0900

    Update rolelist.js

--- commands/rolelist.js
+++ commands/rolelist.js
@@ -10,7 +10,7 @@ const msgs = require("../rolelist.json")
 module.exports.run = async(bot, message, args) =>{
     let messages = msgs[0].message;
     console.log(`${message.guild.roles}`);
-
+    console.log("testing");
     console.log(`${messages}`);
 }
 
