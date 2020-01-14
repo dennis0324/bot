03ecbefbf348c3a39130a82b90ddbe8ec7e6d396
dennis0324 <dennis0324@naver.com>
2020-01-15 07:45:44 +0900

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -5,7 +5,7 @@ const prefix = botconfig.prefix;
 
 module.exports.run = async(bot, message, args) =>{
     if(!args[0]) return message.channel.send("역할을 반드시 적으셔야 합니다.");
-    
+    console.log(typeof args[0]);
     
 }
 
@@ -14,6 +14,6 @@ module.exports.config = {
     name: "join",
     aliases: ["j"],
     description: "필요한 역할을 부여받을 수 있습니다.",
-    usage: "--join <게임 이름>",
+    usage: "--join <목록 이름|숫자>",
     accessableby:"모든 이"
 }
