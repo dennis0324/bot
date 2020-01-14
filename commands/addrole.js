869ab8e669a3700bd7803c6c964ca43839ad2567
dennis0324 <dennis0324@naver.com>
2020-01-15 02:01:51 +0900

    Update addrole.js

--- commands/addrole.js
+++ commands/addrole.js
@@ -11,7 +11,16 @@ module.exports.run = async(bot, message, args) =>{
     if(!args[0]) return message.channel.send("역할을 반드시 적으셔야 합니다.");
     let rolename = args[0];
     console.log(`${rolename}를 생성하려 시도중...  ${msgs}`);
-    
+    var count = 0;
+
+    if(!msgs){
+        count = 0;
+    }
+    else {
+        count = msgs.size;
+    }
+    console.log(`count의 사이즈는${count}`);
+
     let roleAdding = message.guild.roles.find(r => r.name === `${rolename}`);
 
     if(!roleAdding) {
@@ -30,6 +39,7 @@ module.exports.run = async(bot, message, args) =>{
                     SPEAK: true
                 })
             });
+
             message.delete();
             let addRoleEmbed = new Discord.RichEmbed()
             .setColor(Color.lightblue)
@@ -37,6 +47,12 @@ module.exports.run = async(bot, message, args) =>{
             .setDescription(`${rolename}역할이 성공적으로 추가되었습니다.`)
             .setFooter("도우미",null)
             message.channel.send(addRoleEmbed)
+            msgs[count] = {
+                message: rolename
+            }
+            fs.writeFile("./rolelist.json",JSON.stringify (msgs,null,4)), err =>{
+                if(err) throw err;
+            }
         }
         catch(e){
             console.log(e.stack);
