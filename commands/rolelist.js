c90991178ad58c6b65960c4ebcead2d5a6085442
dennis0324 <dennis0324@naver.com>
2020-01-15 06:33:58 +0900

    Update rolelist.js

--- commands/rolelist.js
+++ commands/rolelist.js
@@ -11,7 +11,7 @@ const msgst = require("../rolelist.json");
 module.exports.run = async(bot, message, args) =>{
     let messages = msgst[0].message;
     console.log(`${messages}`);
-    console.log(`${msgst.size}`)
+    console.log(`${msgst[0].size}`)
     let Sembed = new Discord.RichEmbed()
         .setColor(Color.mint)
         .setAuthor(`참여방 목록`,message.guild.iconURL)
