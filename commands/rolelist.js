63033d6d3673eafc75cb9cf041b9b158f7a0fdbb
dennis0324 <dennis0324@naver.com>
2020-01-15 04:54:39 +0900

    test

--- commands/rolelist.js
+++ commands/rolelist.js
@@ -20,7 +20,7 @@ module.exports.run = async(bot, message, args) =>{
             //아직 파일 입출력 못 고침
 
             fs.writeFile("./rolelist.json",JSON.stringify (msgst,null,4), function(err) {
-                if(err) throw err;
+                if(err) console.log('error',err);
             })
             console.log(`#${count} : ${testing}`);
             count++;
