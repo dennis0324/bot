447487ea9922498dd98046ad40b180da50a2893e
dennis0324 <dennis0324@naver.com>
2020-01-15 03:13:41 +0900

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports = bot =>{
     console.log(`${bot.user.username} is online`);
-    let roletesting = bot.user.guild.roles.find(r => r.name === 'testing-pro')
+    let roletesting = bot.user.guild.role.find(r => r.name === 'testing-pro')
     console.log(`${roletesting}`);
     // bot.user.setActivity("명령어에 귀 기울이는 중",{type: "STREAMING"});
     //
