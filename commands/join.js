e4f108f6f08fec4b631850c4ec7f25003411acc9
dennis0324 <dennis0324@naver.com>
2020-01-16 17:18:30 +0900

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -21,26 +21,32 @@ module.exports.run = async(bot, message, args) =>{
         roleAdding = message.guild.roles.find(r => r.name === `${msgst[checknum].message}-pro`);
         rolename = `${msgst[checknum].message}-pro`
     }
-    if(!message.member.roles.find(r => r.name === rolename ) && !roleAdding){
-        console.log("이름을 찾지 못하였습니다.")
-        let embed = new Discord.RichEmbed()
-        .setDescription(`${string}존재하지 않습니다.`);
-        message.channel.send(embed);
-        return ;
-    }
-    if(message.member.roles.find(r => r.name === rolename )){
-        let embed = new Discord.RichEmbed()
-        .setDescription(`이미 ${string}방에 들어와있습니다.`);
-        message.channel.send(embed);
-        return ;
-    }
-    message.member.addRole(roleAdding.id).then(() =>{
-        console.log("successed!");
-    })
 
-    let embed = new Discord.RichEmbed()
-    .setDescription(`${message.author.username}가 ${string}방에 참여했습니다.`);
-    message.channel.send(embed);
+    if(!args[1]){
+        if(!message.member.roles.find(r => r.name === rolename ) && !roleAdding){
+            console.log("이름을 찾지 못하였습니다.")
+            let embed = new Discord.RichEmbed()
+            .setDescription(`${string}존재하지 않습니다.`);
+            message.channel.send(embed);
+            return ;
+        }
+        if(message.member.roles.find(r => r.name === rolename )){
+            let embed = new Discord.RichEmbed()
+            .setDescription(`이미 ${string}방에 들어와있습니다.`);
+            message.channel.send(embed);
+            return ;
+        }
+        message.member.addRole(roleAdding.id).then(() =>{
+            console.log("successed!");
+        })
+        
+        let embed = new Discord.RichEmbed()
+        .setDescription(`${message.author.username}가 ${string}방에 참여했습니다.`);
+        message.channel.send(embed);
+    }
+    else{
+        
+    }
 }
 
 
