c2bbaa66d043ff2fc971f688c6e297d11e2cd7c1
Foxstar <50710829+dennis0324@users.noreply.github.com>
2020-12-22 23:20:18 +0900

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -32,7 +32,7 @@ module.exports.run = async(bot, message, args) =>{
     console.log("testing line #1");
     message.member.roles.cache.find.foreach(usering =>{
         console.log(usering);
-    }
+    });
     if(!message.member.roles.cache.find(r => r.name === rolename ) && !roleAdding){
         console.log("이름을 찾지 못하였습니다.")
         let embed = new Discord.RichEmbed()
