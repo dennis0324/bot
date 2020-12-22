ab14acf5e7a71467e16a3399a3e9c7296517d6f2
Foxstar <50710829+dennis0324@users.noreply.github.com>
2020-12-22 23:52:37 +0900

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -29,7 +29,7 @@ module.exports.run = async(bot, message, args) =>{
         rolename = `${msgst[checknum].message}-pro`
     }
     console.log("testing line #1");
-    console.log(message.member.roles);
+    console.log(message.guild.roles);
     message.guild.roles.cache.mapValues(user => console.log(user));
     if(!message.member.roles.cache.find(r => r.name === rolename ) && !roleAdding){
         console.log("이름을 찾지 못하였습니다.")
