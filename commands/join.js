29e6d215c71efa838846e8cb9e856e50049c1757
Foxstar <50710829+dennis0324@users.noreply.github.com>
2020-12-23 23:53:06 +0900

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -33,6 +33,7 @@ module.exports.run = async(bot, message, args) =>{
     console.log("log 1");
     
     console.log(!roleAdding);
+    console.log(message.member.roles.cache.find(r => r.name === rolename ))
     
     if(!roleAdding){
         console.log("이름을 찾지 못하였습니다.")
