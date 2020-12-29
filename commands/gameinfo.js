b021f94828120f20b2a5c9598f0e41f9c7ba4fb4
Foxstar <50710829+dennis0324@users.noreply.github.com>
2020-12-29 23:31:31 +0900

    Update gameinfo.js

--- commands/gameinfo.js
+++ commands/gameinfo.js
@@ -6,7 +6,13 @@ const prefix = botconfig.prefix;
 
 const msgs = require("../rolelist.json")
 
-
+/*
+    명령어 사용 목적: 추후에 게임 사용시 자동으로 채널 변경하기 위한 목적
+    명령어: --channelchange
+    arg[0]: auto arg[1]: on/off
+            alarm arg[1]: on/off
+            
+*/
 module.exports.run = async(bot, message, args) =>{
-
+    if(message.member
 }
