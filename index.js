75dd80614ec536d464deda759472ce09e0fa61c6
Foxstar <50710829+dennis0324@users.noreply.github.com>
2020-12-29 21:54:11 +0900

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ bot.on("message", message => {
 //     console.log(cmd);
     
 //     let args = messageArray.slice(1);
-    console.log(message.guild.roles.cache.array());
+    console.log(message.guild.roles.cache);
     let role = message.guild.roles.cache.find(r => r.name === 'Minecraft-pro');
     console.log(role);
     // Let's pretend you mentioned the user you want to add a role to (!addrole @user Role Name):
