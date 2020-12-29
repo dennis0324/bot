0af0cee480e1a0f0e7aee8ba269dd5de4f127782
Foxstar <50710829+dennis0324@users.noreply.github.com>
2020-12-29 23:26:54 +0900

    Create gameinfo.js

---
+++ command/gameinfo.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+const Color = require("../colours.json");
+const botconfig = require("../botconfig.json");
+const fs = require('fs');
+const prefix = botconfig.prefix;
+
+const msgs = require("../rolelist.json")
+
+
+module.exports.run = async(bot, message, args) =>{
+
+}
