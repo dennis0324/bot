37499142742b49a0d85ded292675ab071830dd01
Foxstar <50710829+dennis0324@users.noreply.github.com>
2020-12-30 22:54:41 +0900

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -46,9 +46,7 @@ module.exports.run = async (bot, message, args) =>{
 
         message.author.send(Sembed);
         
-        message.author.send("1.roles");
-        message.author.send("2.roles");
-        message.author.send("3.roles");
+        message.author.send("1.roles\n2.testing2\n3.testing3");
         
     }
 }
