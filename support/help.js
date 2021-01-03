ed6d145df09956916432c8965cb70c28aa36cf7e
Foxstar <50710829+dennis0324@users.noreply.github.com>
2021-01-03 23:54:00 +0900

    Update help.js

--- support/help.js
+++ support/help.js
@@ -5,7 +5,8 @@ const prefix = botconfig.prefix;
 
 exports.run = function(bot, message, args, filesName){
     console.log(filesName[args]);
-    if(filesName > args){
-        return "testing";
+    if(filesName < args){
+        message.channel.send("```목록에 있는 숫자를 사용하여주십시오```");
+        return;
     }
 }
