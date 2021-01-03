f39448982c8125a6314f3f81064265a38eb15622
Foxstar <50710829+dennis0324@users.noreply.github.com>
2021-01-03 23:32:42 +0900

    Update help.js

--- support/help.js
+++ support/help.js
@@ -3,4 +3,6 @@ const Color = require("../colours.json");
 const botconfig = require("../botconfig.json");
 const prefix = botconfig.prefix;
 
-
+exports.add = function(bot, message, args){
+    console.log(args);
+}
