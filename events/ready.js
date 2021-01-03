7a68197be78eb75dc0423f63c4bf5fe5dcb4af53
Foxstar <50710829+dennis0324@users.noreply.github.com>
2021-01-03 22:23:48 +0900

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -6,7 +6,7 @@ const msgst = require("../rolelist.json");
 module.exports = bot =>{
     console.log(`${bot.user.username} is online`);
     console.log(process.env.CLEARDB_DATABASE_URL);
-    bot.guilds.cache.each(r => console.log(r.username + "all")).filter(user => user.bot).each(user => console.log(user.username + "bot"));
+    console.log(bot.guilds.cache.array.length);
     
     bot.guilds.cache.forEach(element => {
         var count = 0;
