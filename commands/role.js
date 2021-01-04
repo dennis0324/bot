9240a8ee1a14e9b911606091551f395f9d066cb6
Foxstar <50710829+dennis0324@users.noreply.github.com>
2021-01-04 23:01:37 +0900

    Update role.js

--- commands/role.js
+++ commands/role.js
@@ -13,7 +13,7 @@ const role_remove = require("../support/role_remove.js");
 //when user enter 'role' command this module will be executed
 module.exports.run = async(bot, message, args) =>{
     if(!args[0]){
-        message.channel.send(`\`\`\`ml\n \`옵션\`을 입력해주세요\n\`\`\``);
+        message.channel.send(`\`\`\`ml\n. \`옵션\`을 입력해주세요\n\`\`\``);
         let addRoleEmbed = new Discord.MessageEmbed()
             .setColor(Color.lightblue)
             .setAuthor(`알림`,"")
