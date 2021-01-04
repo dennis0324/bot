cdf1614f2b0d19f334c6285ff7a3749ae665043b
Foxstar <50710829+dennis0324@users.noreply.github.com>
2021-01-04 21:58:44 +0900

    Update help.js

--- support/help.js
+++ support/help.js
@@ -3,11 +3,15 @@ const Color = require("../colours.json");
 const botconfig = require("../botconfig.json");
 const prefix = botconfig.prefix;
 
+/*
+this function
+*/
 exports.run = function(bot, message, args, filesName){
     console.log(filesName[args]);
     console.log(filesName.length);
     console.log(args);
     if(filesName < args){
+        console.log(message.channel.author);
         message.channel.SendMessage('```없는 선택지입니다. ```');
         return;
     }
