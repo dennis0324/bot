9d3cfca643c18da87910154e3ece10be433f7fdc
Foxstar <50710829+dennis0324@users.noreply.github.com>
2021-01-04 22:09:06 +0900

    Update help.js

--- support/help.js
+++ support/help.js
@@ -4,13 +4,13 @@ const botconfig = require("../botconfig.json");
 const prefix = botconfig.prefix;
 
 /*
-this function
+this function is to help user to get command from help command
 */
 exports.run = function(bot, message, args, filesName){
     console.log(filesName[args]);
     console.log(filesName.length);
     console.log(args);
-    if(filesName < args){
+    if(filesName.length < args){
         console.log(message.channel.author);
         message.channel.SendMessage('```없는 선택지입니다. ```');
     }
