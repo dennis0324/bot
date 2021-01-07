a92578a18228f1e7915b82b281454089834ff378
Foxstar <50710829+dennis0324@users.noreply.github.com>
2021-01-07 22:25:55 +0900

    Create role_list.js

---
+++ support/role_list.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+const Color = require("../colours.json");
+const botconfig = require("../botconfig.json");
+const fs = require('fs');
+const prefix = botconfig.prefix;
+const msgst = require("../rolelist.json");
+
+
+
+
+module.exports.run = async(bot, message, args) =>{
+    var array = "";
+    let messages = msgst[0].message;
+    let count_num = msgst.size;
+    console.log(`${count_num}`);
+       for(var i = 0; i < count_num; i++){
+           array += `${i}. ${msgst[i].message}\n`;
+
+       }
+
+
+        message.channel.send(`\`\`\`md\n* 참여 목록:\n${array}\n\`\`\``);
+}
+
+
+module.exports.config = {
+    name: "rolelist",
+    aliases: ["rl","rolel"],
+    description: "선택 가능한 역할을 볼 수 있습니다.",
+    usage: "--rolelist",
+    accessableby:"모든 이"
+}
