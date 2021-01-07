bc8d47559ce605bfa4ebbaad82f3f66dc1cb6e7b
Foxstar <50710829+dennis0324@users.noreply.github.com>
2021-01-07 22:02:57 +0900

    easy creator for embedMessage

--- support/embedHelper.js
+++ support/embedHelper.js
@@ -1,3 +1,14 @@
 Const embedHelper = class{
-
+    constructor(){}
+    
+    set(title, descript, color){
+        return new Discord.MessageEmbed()
+            .setColor(color)
+            .setAuthor(title,"")
+            .setDescription(descript)
+    }
+    
+    show(message, embed){
+        message.channel.send(embed);
+    }
 }
