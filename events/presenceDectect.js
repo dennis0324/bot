059e4b33b184c59b35601fa348bf7041e63b7beb
Foxstar <50710829+dennis0324@users.noreply.github.com>
2021-01-10 22:40:36 +0900

    Update presenceDectect.js

--- events/presenceDectect.js
+++ events/presenceDectect.js
@@ -7,6 +7,7 @@ const prefix = botconfig.prefix;
 
 client.on("presenceUpdate", (oldPresence, newPresence) => {
     if (!newPresence.activities) return false;
+    console.log(newPresence);
     newPresence.activities.forEach(activity => {
         if (activity.type == "STREAMING") {
             console.log(`${newPresence.user.tag} is streaming at ${activity.url}.`);
