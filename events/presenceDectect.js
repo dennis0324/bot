85c62322da4886af405b6ad572487532f1d97d3f
Foxstar <50710829+dennis0324@users.noreply.github.com>
2021-01-10 22:24:46 +0900

    Create presenceDectect.js

---
+++ events/presenceDectect.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+const Color = require("../colours.json");
+const botconfig = require("../botconfig.json");
+const prefix = botconfig.prefix;
+
+
+
+client.on("presenceUpdate", (oldPresence, newPresence) => {
+    if (!newPresence.activities) return false;
+    newPresence.activities.forEach(activity => {
+        if (activity.type == "STREAMING") {
+            console.log(`${newPresence.user.tag} is streaming at ${activity.url}.`);
+        };
+    });
+});
