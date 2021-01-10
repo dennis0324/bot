1bb8ad3997a9d45d87d26af4f3d42d9bd6286775
Foxstar <50710829+dennis0324@users.noreply.github.com>
2021-01-10 22:15:36 +0900

    Update help.js

--- support/help.js
+++ support/help.js
@@ -25,7 +25,7 @@ exports.run = function(bot, message, args, filesName){
         command = bot.commands.get(commands);
         var specificHelpEmbed = new Discord.MessageEmbed()
         .setColor(Color.mint)
-        .setAuthor("",message.guild.iconURL)
+        .setAuthor("알림",null)
         .setDescription(`봇 수식: ${prefix}\n\n**명령:** ${command.config.name}\n**부가설명:** ${command.config.description || "설명 없음"}\n**사용:** ${command.config.usage || "사용 없음"}\n**사용 가능한 역할:** ${command.config.accessableby || "Everyone"}\n**또 다른 명령어:** ${command.config.noalias || command.config.aliases}`)
         return message.channel.send({embed: specificHelpEmbed});
     }
