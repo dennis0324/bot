9759589f9335a687ef454fb62be4c22ff1940ae3
Foxstar <50710829+dennis0324@users.noreply.github.com>
2021-01-13 22:05:51 +0900

    Update presenceUpdate.js

--- events/presenceUpdate.js
+++ events/presenceUpdate.js
@@ -2,9 +2,12 @@ const Discord = require("discord.js");
 const Color = require("../colours.json");
 const botconfig = require("../botconfig.json");
 const prefix = botconfig.prefix;
-
+/*
+event that catch when discord user status, or activities changes
+*/
 module.exports = bot =>{
     bot.on("presenceUpdate", (oldPresence, newPresence) => {
+        conosole.log("testing");
         if (!newPresence.activities) return false;
         newPresence.activities.forEach(activity => {
             if (activity.type == "STREAMING") {
