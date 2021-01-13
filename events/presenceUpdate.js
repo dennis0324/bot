648b4edd37d5c3d04c84e1fb9acf76609b6a4480
Foxstar <50710829+dennis0324@users.noreply.github.com>
2021-01-13 22:01:34 +0900

    Update presenceUpdate.js

--- events/presenceUpdate.js
+++ events/presenceUpdate.js
@@ -6,7 +6,6 @@ const prefix = botconfig.prefix;
 module.exports = bot =>{
     bot.on("presenceUpdate", (oldPresence, newPresence) => {
         if (!newPresence.activities) return false;
-        console.log(newPresence);
         newPresence.activities.forEach(activity => {
             if (activity.type == "STREAMING") {
                 console.log(`${newPresence.user.tag} is streaming at ${activity.url}.`);
