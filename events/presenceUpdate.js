d170410d842e43a0b96d37774fa9251677874503
Foxstar <50710829+dennis0324@users.noreply.github.com>
2021-01-13 22:14:45 +0900

    Update presenceUpdate.js

--- events/presenceUpdate.js
+++ events/presenceUpdate.js
@@ -7,12 +7,17 @@ event that catch when discord user status, or activities changes
 */
 module.exports = bot =>{
     bot.on("presenceUpdate", (oldPresence, newPresence) => {
-        console.log("testing");
+        if(olePresence.userID == newPresence.userID) return;
         if (!newPresence.activities) return false;
         newPresence.activities.forEach(activity => {
             if (activity.type == "STREAMING") {
                 console.log(`${newPresence.user.tag} is streaming at ${activity.url}.`);
             };
+        });
+        newPresence.activities.forEach(activity => {
+            if( activity.type == "PLAYING"){
+                console.log("he is playing");
+            }
         });
     });
 }
