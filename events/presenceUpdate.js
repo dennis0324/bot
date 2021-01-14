56e1f8ea61f2fcb3d4444b21332c8f353899736e
Foxstar <50710829+dennis0324@users.noreply.github.com>
2021-01-14 23:27:38 +0900

    Update presenceUpdate.js

--- events/presenceUpdate.js
+++ events/presenceUpdate.js
@@ -7,16 +7,17 @@ event that catch when discord user status, or activities changes
 */
 module.exports = bot =>{
     bot.on("presenceUpdate", (oldPresence, newPresence) => {
-        if (oldPresence.status !== newPresence.status) {
-            console.log(`${newPresence.user.tag} user is now ${newPresence.status} in guild ${newPresence.guild.name}`);
-            console.log(`old-> ${oldPresence.user.username} : ${oldPresence.activities.name} : ${oldPresence.createAt}`);
-            console.log(`new-> ${newPresence.user.username} : ${newPresence.activities.name} : ${newPresence.createAt}`);
-        }
+//        console.log(`${newPresence.user.tag} user is now ${newPresence.status} in guild ${newPresence.guild.name}`);
+//        if (oldPresence.status !== newPresence.status) {
+//            console.log(`${newPresence.user.tag} user is now ${newPresence.status} in guild ${newPresence.guild.name}`);
+//            console.log(`old-> ${oldPresence.user.username} : ${oldPresence.activities.name} : ${oldPresence.createAt}`);
+//            console.log(`new-> ${newPresence.user.username} : ${newPresence.activities.name} : ${newPresence.createAt}`);
+ //       }
         if (oldPresence.clientStatus !== newPresence.clientStatus) return;
         if (newPresence.member.user.bot) return;
         if (!newPresence.activities) return false;
         newPresence.activities.forEach(activity => {
-            if( activity.type == "CUSTOM_STATUS"){ //need to change to Playing,STREAMING, etc. except custom_status using temp beacause of military
+            if( activity.type == "PLAYING"){ //need to change to Playing,STREAMING, etc. except custom_status using temp beacause of military
                 console.log(activity.name);
                 bot.channels.cache.forEach(c => {
                     if(c.type != "category"){//to exclude category channels
