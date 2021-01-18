12f571a27aff71c2dd73fb57898065e95306bef0
Foxstar <50710829+dennis0324@users.noreply.github.com>
2021-01-18 22:12:16 +0900

    Update presenceUpdate.js

--- events/presenceUpdate.js
+++ events/presenceUpdate.js
@@ -7,27 +7,22 @@ event that catch when discord user status, or activities changes
 */
 module.exports = bot =>{
     bot.once("presenceUpdate", (oldPresence, newPresence) => {
-//        console.log(`${newPresence.user.tag} user is now ${newPresence.status} in guild ${newPresence.guild.name}`);
-//        if (oldPresence.status !== newPresence.status) {
-//            console.log(`${newPresence.user.tag} user is now ${newPresence.status} in guild ${newPresence.guild.name}`);
-//            console.log(`old-> ${oldPresence.user.username} : ${oldPresence.activities.name} : ${oldPresence.createAt}`);
-//            console.log(`new-> ${newPresence.user.username} : ${newPresence.activities.name} : ${newPresence.createAt}`);
- //       }
-//        if (oldPresence.clientStatus !== newPresence.clientStatus) return;
-        if (newPresence.member.user.bot) return;
-        if (!newPresence.activities) return false;
+        if (newPresence.member.user.bot) return; //if bot change presence return
+        if (!newPresence.activities) return false; //if newPresence is null return
+        
         newPresence.activities.forEach(activity => {
             if( activity.type == "PLAYING"){ //need to change to Playing,STREAMING, etc. except custom_status using temp beacause of military
-                console.log(newPresence.user.tag);
+                console.log("presenceUpdate: ",newPresence.user.tag);
                 console.log(activity.name.toLowerCase());
                 
                 let startingGame = newPresence.client.channels.cache.find(c => activity.name.toLowerCasec.includes(c.name.toLowerCase) && c.type !== "category");
-                console.log(startingGame.name);
+                console.log("presenceUpdate: ", startingGame.name);
                 if(startingGame) {
-                    console.log("find game... searching for user's voice channel connection");
+                    console.log("presenceUpdate: find game... searching for user's voice channel connection");
                 }
                 else{
                     concole.log("nothing has found");
+                    return;
                 }
                 bot.channels.cache.forEach(c => {
                     if(c.type != "category"){//to exclude category channels
